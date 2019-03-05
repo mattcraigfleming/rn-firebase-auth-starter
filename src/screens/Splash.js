9ec3e1f226d96dd72e07eb522d9a43bc0bec80ab
Matt Fleming <matt.fleming@greatstate.co>
2019-03-05 17:30:46 +0000

    Initial project setup

---
+++ src/screens/Splash.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import { View, Text, ActivityIndicator, StyleSheet } from 'react-native'
+
+class Splash extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Splash</Text>
+        <ActivityIndicator size="large" />
+      </View>
+    )
+  }
+}
+
+export default Splash
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  }
+})
